6238ae77d48588a679091d362badf7c586ae2815
foxdev <foxdev@pdx.edu>
2022-01-29 19:07:07 -0800

    updated midi open/close function

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,53 @@
 //! 
 //! Devon Fox 2022
 
+use midir::*;
+//use rand::Rng;
+use std::error::Error;
+use std::io::{stdin, stdout, Write};
+use std::thread::sleep;
+use std::time::Duration;
+
+/// Scans midi ports and lets user connect to port of choice, if available
+/// If no ports available, returns an error.
+/// Sourced from the 'midir' crate 'test_play.rs' example
+pub fn run() -> Result<(), Box<dyn Error>> {
+    let midi_out = MidiOutput::new("My Test Output")?;
+
+    // Get an output port (read from console if multiple are available)
+    let out_ports = midi_out.ports();
+    let out_port: &MidiOutputPort = match out_ports.len() {
+        0 => return Err("no output port found".into()),
+        1 => {
+            println!(
+                "Choosing the only available output port: {}",
+                midi_out.port_name(&out_ports[0]).unwrap()
+            );
+            &out_ports[0]
+        }
+        _ => {
+            println!("\nAvailable output ports:");
+            for (i, p) in out_ports.iter().enumerate() {
+                println!("{}: {}", i, midi_out.port_name(p).unwrap());
+            }
+            print!("Please select output port: ");
+            stdout().flush()?;
+            let mut input = String::new();
+            stdin().read_line(&mut input)?;
+            out_ports
+                .get(input.trim().parse::<usize>()?)
+                .ok_or("invalid output port selected")?
+        }
+    };
+
+    println!("\nOpening connection");
+    let conn_out = midi_out.connect(out_port, "midir-test")?;
+    println!("Connection open.");
+    // put midi generation menu and/or functions here
+    sleep(Duration::from_millis(150));
+    println!("\nClosing connection");
+    // This is optional, the connection would automatically be closed as soon as it goes out of scope
+    conn_out.close();
+    println!("Connection closed");
+    Ok(())
+}
