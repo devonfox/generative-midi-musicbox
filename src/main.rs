64b265c5b3e974987c6cdce0d7f90bb6db6014d4
foxdev <foxdev@pdx.edu>
2022-02-24 22:07:46 -0800

    todo: implement testing

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use generative_midi_musicbox::*;
 // prophet patch for ambient testing -> U4 P69
 
 fn main() {
+    // Driver for running lib code
     match run() {
         Ok(_) => (),
         Err(err) => println!("Error: {}", err),
