f9c7bc6fd52566ff9241834b6a0bf2ae00c4df07
foxdev <foxdev@pdx.edu>
2022-03-13 21:19:41 -0700

    updated to take args

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@
 //! Devon Fox 2022
 
 use generative_midi_musicbox::*;
+use std::env;
 
 // prophet patch for ambient testing -> U4 P69
 
 fn main() {
     // Driver for running lib code
-    match run() {
+    let args: Vec<String> = env::args().collect();
+    match run(args) {
         Ok(_) => (),
         Err(err) => println!("Error: {}", err),
     }
